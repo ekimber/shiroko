f825b998b6c6d70bfc86ee5613cc2aedfc75dfcf
ekimber <edward.kimber@gmail.com>
2014-04-02 00:32:36 +0100

    working snapshot writes

--- src/shiroko/core.clj
+++ src/shiroko/core.clj
@@ -4,58 +4,57 @@
   (:use clojure.java.io)
   (:use clojure.tools.logging)
   (:require [clojure.string :refer [join split]])
-  (:require [clojure.core.async :as async :refer [put! >!! <!! chan go go-loop map< thread close! pipe]])
+  (:require [clojure.core.async :as async :refer [put! >!! <!! chan go go-loop map< thread close! mult tap untap filter<]])
   (:require [clj-time.format :as f])
   (:require [clj-time.local :as l])
   (:require [clojurewerkz.serialism.core :as s]))
 
 (def txn-counter (atom 0M))
-(def journal-ch (chan))
+
+(defn ^:private enumerate-txn [txn]
+  (assoc txn :id (swap! txn-counter inc)))
 
 (defn ^:private clj-serialize
   "Serialize args as ;-separated clojure strings"
   [& items]
   (join ";" (map #(s/serialize % :clojure) items)))
 
-(defn write-txn [txn]
-  (>!! journal-ch (clj-serialize (:fn txn) (:args txn) (swap! txn-counter inc)))
-  txn)
+(defn ^:private serialize-txn [txn]
+  (clj-serialize (:fn txn) (:args txn) (:id txn)))
 
-(def input-ch (chan 5))
-(def exec-ch (map< write-txn input-ch))
+(def input-ch (map< enumerate-txn (chan 5)))
+(def input-mult (mult input-ch))
+(def exec-ch (tap input-mult (chan)))
+(def journal-channel (map< serialize-txn (tap input-mult (chan))))
+(def snapshot-await (chan))
+(def exec-await (chan))
 
-(defn txn-executor [ch]
+(defn ^:private txn-executor [ch]
   (go-loop []
     (let [txn (<! ch)]
       (try
-        (>! (:ret txn) (dosync (apply (:fn txn) (:args txn))))
+        (put! (:ret txn) (dosync (apply (:fn txn) (:args txn))))
         (catch Exception e
           (warn e "Exception thrown while executing transaction.")
-          (close! (:ret txn)))))
+          (close! (:ret txn))))
+      (>! exec-await (:id txn))
+      (<! snapshot-await))
     (recur)))
-
-(txn-executor exec-ch)
-
-(defn apply-transaction
-  "Persist and apply a transaction, returning a channel that will contain the result. "
-  [txn-fn & args]
-  (assert (fn? txn-fn) "Transaction to apply must be a fn.")
-  (let [c (chan 1)]
-    (put! input-ch {:fn txn-fn :args args :ret c})
-    c))
 
 (defn ^:private write-to-stream [w txn]
   (.write w (str txn "\n"))
   (.flush w))
 
-(defn ^:private txn-writer [ch filename]
-  (with-open [w (writer filename)]
-    (loop []
-      (let [txn (<!! ch)]
-        (when txn (write-to-stream w txn) (recur))))))
+(defn ^:private txn-writer [filename]
+  (thread
+    (with-open [w (writer filename)]
+      (loop []
+        (let [txn (<!! journal-channel)]
+          (when txn
+            (write-to-stream w txn)
+            (recur)))))))
 
 ;; Journal reading
-
 (defn ^:private ingest
   "Update the transaction counter and execute a transaction."
   [[f args txn-id]]
@@ -81,6 +80,31 @@
 (defn ^:private journal-file-name [data-dir txn-number]
   (str data-dir "/" txn-number ".journal"))
 
+;; Snapshot writing
+(defn write-snapshot [snapshot fname]
+  (with-open [w (writer fname)]
+    (.write w snapshot)
+    (.flush w)))
+
+(defn create-snapshot
+  "Serialize the persistent refs as a snapshot that will be loaded on next start."
+  [ref-list]
+  (apply clj-serialize (map deref ref-list)))
+
+(defn snapshot-filename [id data-dir]
+  (str data-dir "/" id ".snapshot"))
+
+(defn snapshot-writer [ref-list data-dir]
+  (println "Taking snapshots")
+  (thread
+    (loop []
+      (let [last-executed-id (<!! exec-await)]
+        (if (zero? (rem last-executed-id 10))
+          (write-snapshot (create-snapshot ref-list) (snapshot-filename last-executed-id data-dir))))
+      (>!! snapshot-await :go)
+      (recur))))
+
+;; Snapshot reading
 (defn init-db
   "Initialise the persistence base, reading and executing all persisted transactions."
   [& {:keys [data-dirname ref-list]
@@ -98,16 +122,17 @@
     (doseq [n (sort (journal-numbers data-dir))]
       (read-journal (journal-file-name data-dir n)))
     ;Start writer
-    (println (if ref-list "Taking snapshots" "No snapshots"))
-    (txn-writer journal-ch (journal-file-name data-dir (inc @txn-counter)))))
+    (if ref-list (snapshot-writer ref-list data-dir))
+    (txn-writer (journal-file-name data-dir (inc @txn-counter)))
+    (txn-executor exec-ch)))
 
-;; Snapshot writing
-(defn write-snapshot
-  "Serialize the persistent refs as a snapshot that will be loaded on next start."
-  [ref-list]
-  (apply clj-serialize (map deref ref-list)))
-
-;; Snapshot reading
+(defn apply-transaction
+  "Persist and apply a transaction, returning a channel that will contain the result. "
+  [txn-fn & args]
+  (assert (fn? txn-fn) "Transaction to apply must be a fn.")
+  (let [c (chan)]
+    (put! input-ch {:fn txn-fn :args args :ret c})
+    c))
 
 ;; APP CODE
 (def msgs (ref []))
@@ -123,10 +148,7 @@
   "I don't do a whole lot ... yet."
   [& args]
   (init-db :ref-list [msgs])
-  (dotimes [n 3]
+  (dotimes [n 13]
     (println (<!! (apply-transaction write-msg "Bob" n (now-str)))))
-  (Thread/sleep 300)
-  (write-snapshot [msgs])
-  (close! journal-ch)
-  (close! input-ch)
+  (Thread/sleep 500)
 )
