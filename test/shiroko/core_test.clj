b1ea48fcd9d5aee276f49182b84c3deeccc633cd
ekimber <edward.kimber@gmail.com>
2014-04-06 09:17:12 +0100

    test fixture common setup

--- test/shiroko/core_test.clj
+++ test/shiroko/core_test.clj
@@ -16,23 +16,29 @@
   (doseq [f (file-seq (file "testbase"))]
     (delete-file f true)))
 
+(defn db-setup [f]
+  (dosync (ref-set x 2))
+  (init-db :data-dirname "testbase")
+  (f)
+  (delete-test-files))
+
+(use-fixtures :each db-setup)
+
 (deftest simple-transaction
   (testing "Simple increment transaction"
-    (dosync (ref-set x 2))
-    (delete-test-files)
-    (init-db :data-dirname "testbase")
     (is (= 3 (<!! (apply-transaction increment-x))))
     (is (= 3 @x))))
 
 (deftest transaction-fails
   (testing "Failing transaction closes channel"
-    (delete-test-files)
-    (init-db :data-dirname "testbase")
     (is (nil? (<!! (apply-transaction bad-txn))))))
 
 (deftest test-snapshot
   (testing "Writing/reading a snapshot")
-    (dosync (ref-set x 2))
     @(write-snapshot (create-snapshot [x]) "11.snapshot")
     (is (= '(2) (read-snapshot "." 11)))
     (delete-file (file "11.snapshot")))
+
+;(deftest stress-test
+;  (testing "1 million transaction test")
+;  (dosyn
